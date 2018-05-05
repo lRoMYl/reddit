2383a1df09176a3cc135a9783a66fd10ada45509
Romy <romy@myboost.com.my>
2018-05-05 18:31:42 +0800

    Added new empty iOS project

---
+++ reddit/reddit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  reddit
+//
+//  Created by Cheah Bee Kim on 5/5/18.
+//  Copyright © 2018 Cheah Bee Kim. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
